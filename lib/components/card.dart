d5b106c24140440c33fb2811aa1975896a742f3c
Muhammadamin Raufjonov <raufjonovmuhammadamin@gmail.com>
2025-07-31 23:14:09 +0500

    done

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:test_task/domain/json.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:test_task/domain/json.dart';
 
-class CharacterCard extends StatelessWidget {
+class CharacterCard extends StatefulWidget {
   final RickMorty character;
   final bool isFavorite;
   final VoidCallback onFavoriteToggle;
@@ -13,6 +13,45 @@ class CharacterCard extends StatelessWidget {
     required this.isFavorite,
     required this.onFavoriteToggle,
   });
+
+  @override
+  State<CharacterCard> createState() => _CharacterCardState();
+}
+
+class _CharacterCardState extends State<CharacterCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 0.8,
+      end: 1.2,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  void _handleFavoriteTap() {
+    if (!_controller.isAnimating) {
+      _controller.forward().then((_) => _controller.reverse());
+      widget.onFavoriteToggle();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  TextStyle _textStyle() =>
+      const TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +66,7 @@ class CharacterCard extends StatelessWidget {
             ClipRRect(
               borderRadius: BorderRadius.circular(8),
               child: CachedNetworkImage(
-                imageUrl: character.image,
+                imageUrl: widget.character.image,
                 width: 80,
                 height: 80,
                 fit: BoxFit.cover,
@@ -41,52 +80,31 @@ class CharacterCard extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    character.name,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                  Text(widget.character.name, style: _textStyle()),
                   const SizedBox(height: 8),
                   Text(
-                    'id: ${character.id.toString()}',
+                    'id: ${widget.character.id}',
                     style: const TextStyle(
                       fontSize: 10,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Text(
-                    character.status,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    character.gender,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    character.species,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                  Text(widget.character.status, style: _textStyle()),
+                  Text(widget.character.gender, style: _textStyle()),
+                  Text(widget.character.species, style: _textStyle()),
                   Align(
                     alignment: Alignment.bottomRight,
-                    child: IconButton(
-                      icon: Icon(
-                        isFavorite
-                            ? Icons.star_purple500_sharp
-                            : Icons.star_purple500_rounded,
-                        color: isFavorite ? Colors.yellow : null,
+                    child: ScaleTransition(
+                      scale: _scaleAnimation,
+                      child: IconButton(
+                        icon: Icon(
+                          widget.isFavorite
+                              ? Icons.star_purple500_sharp
+                              : Icons.star_purple500_rounded,
+                          color: widget.isFavorite ? Colors.yellow : null,
+                        ),
+                        onPressed: _handleFavoriteTap,
                       ),
-                      onPressed: onFavoriteToggle,
                     ),
                   ),
                 ],
